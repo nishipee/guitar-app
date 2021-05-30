a6df5f261945ccf71043a23df3945c47fcb62512
nishipee <sn.haryo.000@gmail.com>
2021-05-30 22:49:31 +0900

    からのメッセージが送信できないバリデーションの設定

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -11,7 +11,7 @@ class MessagesController < ApplicationController
     if @message.save
       redirect_to room_messages_path(@room.id)
     else
-      @messages = @room.messages.ingludes(:user)
+      @messages = @room.messages.includes(:user)
       render :index
     end
   end
